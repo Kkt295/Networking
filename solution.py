0d78ddfcf7ba2d019aec72b7e709f583ea340f29
Kkt295 <78629153+Kkt295@users.noreply.github.com>
2021-02-20 15:40:24 -0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import sys # In order to terminate the program
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
     #Prepare a sever socket
-    serverSocket.bind(("68.6.131.246", port))
+    serverSocket.bind(("", port))
     #Fill in start
     serverSocket.listen(5) #Fill in end
 
