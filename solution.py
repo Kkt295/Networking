41915d64abe623c5726b11f83c68e12ea37bf816
Kkt295 <78629153+Kkt295@users.noreply.github.com>
2021-02-25 21:36:32 -0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,8 +8,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    endmsg = "\r\n.\r\n"
 
    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-   mailserver = "smtp.gmail.com"
-   port = 587
+   #mailserver = "smtp.gmail.com"
+   #port = 587
 
    # Create socket called clientSocket and establish a TCP connection with mailserver and port
    clientSocket = socket(AF_INET, SOCK_STREAM)
